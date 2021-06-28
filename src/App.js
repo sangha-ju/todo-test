77e6d99ae4ec737cb1c9271b7f8f61b86590bc95
sangha ju <sanghaju@sanghaui-MacBookPro.local>
2021-06-28 14:10:13 +0900

    todo head date commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { createGlobalStyle } from "styled-components";
+import TodoHead from "./components/TodoHead";
+import TodoTemplate from "./components/TodoTemplate";
+
+const GlobalStyle = createGlobalStyle`
+ body {
+  background-color: #dfe6e9;
+
+ }
+`;
+
+function App() {
+  return (
+    <div className="App">
+      <GlobalStyle />
+      <TodoTemplate>
+        <TodoHead />
+      </TodoTemplate>
+    </div>
+  );
+}
+
+export default App;
